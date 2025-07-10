203b434dccde3247f4e89540f14bb2e7eb8968c6
muhamsh <muhammad-ashraful@outlook.com>
2025-07-11 00:35:03 +0600

    crafting the project into modular

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,6 +2,7 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { User } from "./user.model";
+import { createUserService } from "./user.service";
 // import { UserServices } from "./user.service";
 
 // const createUserFunction = async (req: Response, res: Response) => {
@@ -21,9 +22,11 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
 
         // createUserFunction(req, res)
 
-        const { name, email } = req.body;
-        console.log(name, email)
-        const user = await User.create( { name, email } );
+        // const { name, email } = req.body;
+        // // console.log(name, email)
+        // const user = await User.create( { name, email } );
+
+        const user = await createUserService( req.body );
 
         res.status( httpStatus.CREATED ).json( {
             message: `user created!!`,
---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,13 @@
+import { IUser } from "./user.interface";
+import { User } from "./user.model";
+
+export const createUserService = async(payload : Partial<IUser>) =>
+{
+    const { name, email } = payload;
+    // console.log(name, email)
+    const user = await User.create( { name, email } );
+
+    console.log( "created a user: ", user );
+
+    return user
+}
