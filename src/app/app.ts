fe18923cfae39791b4213c5071d61c2315e10c76
muhamsh <muhammad-ashraful@outlook.com>
2025-07-11 00:16:12 +0600

    user creation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,24 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import { userRouter } from "./modules/user/user.route";
+import cors from 'cors'
 
 const app: Application = express();
 
-app.use("api/v1/user", userRouter)
+app.use( express.json() )
+app.use(cors())
+
+app.use( "/api/v1/user", userRouter )
+
+
+// global not found route
+app.use( async (req: Response, res: Request, next: NextFunction) =>
+{
+    res.status( 200 ).json( {
+        success: false,
+        status: "error",
+        message: "Route not found!"
+    } );
+} );
 
 app.get( "/", async( req: Response, res: Request, next: NextFunction ) =>
 {
