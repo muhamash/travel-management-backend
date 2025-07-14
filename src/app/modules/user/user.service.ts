cad16b2b7579a8cf3822c5f7abbaac5f423585ab
muhamsh <muhammad-ashraful@outlook.com>
2025-07-14 18:42:08 +0600

    create user with validation oka

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,13 +1,22 @@
-import { IUser } from "./user.interface";
+import bcrypt from "bcryptjs";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
 
 export const createUserService = async(payload : Partial<IUser>) =>
 {
-    const { name, email } = payload;
+    const { email, password, ...rest } = payload;
     // console.log(name, email)
-    const user = await User.create( { name, email } );
 
-    // console.log( "created a user: ", user );
+    const hashedPass = await bcrypt.hash( password as string, 10 );
+    
+    // const isUser
+    const provider: IAuthProvider = {
+        provider: "credentials",
+        providerId: email as string
+    }
+    const user = await User.create( { email, auths: [provider], password: hashedPass, ...rest } );
+
+    console.log( "created a user: ", user, password, hashedPass );
 
     return user
 }
