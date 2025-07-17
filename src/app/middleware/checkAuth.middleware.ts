dc011eee3102329a4df98909f91546d3410981f6
muhamsh <muhammad-ashraful@outlook.com>
2025-07-17 20:16:45 +0600

    updating password oka

--- src/app/middleware/checkAuth.middleware.ts
+++ src/app/middleware/checkAuth.middleware.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from "express";
+import httpStatus from 'http-status-codes';
 import { JwtPayload } from "jsonwebtoken";
 import { envStrings } from "../config/env.config";
 import { AppError } from "../config/errors/App.error";
+import { IsActive } from "../modules/user/user.interface";
+import { User } from "../modules/user/user.model";
 import { verifyToken } from "../utils/middleware.util";
 
 export const checkAuth = ( ...authRoles: string[] ) => async ( req: Request, res: Response, next: NextFunction ) =>
@@ -18,7 +21,19 @@ export const checkAuth = ( ...authRoles: string[] ) => async ( req: Request, res
 
         const verifiedToken = verifyToken( accessToken, envStrings.ACCESS_TOKEN_SECRET ) as JwtPayload;
 
-        // console.log( verifiedToken );
+        const user = await User.findOne( { email: verifiedToken.email } );
+
+        console.log("verified token user", verifiedToken );
+
+        if ( !user )
+        {
+            throw new AppError( httpStatus.NOT_FOUND, `User not found` )
+        }
+
+        if ( user?.isDeleted || user?.isActive !== IsActive.ACTIVE )
+        {
+            throw new AppError( httpStatus.CONFLICT, `User is is restricted from the check auth middleware!!` );
+        }
 
         if ( !authRoles.includes( verifiedToken.role ) )
         {
