f2f7573186b02885ff8fea996dd154103fcf99bb
muhamsh <muhammad-ashraful@outlook.com>
2025-07-25 01:37:22 +0600

    part 6 started

---
+++ src/app/modules/booking/booking.model.ts
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+import { BookingStatus, IBooking } from "./booking.interface";
+
+
+const bookingSchema = new Schema( {
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    payment: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "Payment"
+    },
+    tour: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "Tour"
+    },
+    guestCount: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: Object.values( BookingStatus ),
+        default: BookingStatus.PENDING
+    }
+} );
+
+export const Booking = model<IBooking>( "Booking", bookingSchema );
